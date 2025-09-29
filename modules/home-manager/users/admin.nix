30dc4455525959da608325565608b453d0836bbb
Adam Kijowski <agkijow@gmail.com>
2025-09-29 17:34:03 -0600

    Update flake to 25.05

--- modules/home-manager/users/admin.nix
+++ modules/home-manager/users/admin.nix
@@ -111,7 +111,7 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       hashicorp.terraform
     ];
     # profiles = {
