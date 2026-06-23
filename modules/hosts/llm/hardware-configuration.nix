62e5ebcc007759cb4bc3875ca47cee06c99854f7
Adam Kijowski <agkijow@gmail.com>
2026-06-23 12:03:01 -0600

    llm - refactor nvidia file back to hardware-configuration. Force enable of nvidia drivers and some extra settings.

--- modules/hosts/llm/hardware-configuration.nix
+++ modules/hosts/llm/hardware-configuration.nix
@@ -8,6 +8,9 @@
   imports = [
     # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/profiles/qemu-guest.nix
     (modulesPath + "/profiles/qemu-guest.nix")
+    # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/installer/scan/not-detected.nix
+    # Enables non-free packages (like Nvidia drivers)
+    (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   boot.initrd.availableKernelModules = [
@@ -18,9 +21,21 @@
     "sd_mod"
     "sr_mod"
   ];
-  boot.initrd.kernelModules = ["dm-snapshot"];
+  # force nvidia modules to be loaded
+  # nvidia_udm is managed automatically so should not be installed here
+  # This should have been handled automatically but was not
+  # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/hardware/video/nvidia.nix#L445
+  boot.initrd.kernelModules = ["dm-snapshot" "nvidia" "nvidia_modeset"
+"nvidia_drm"];
   boot.kernelModules = ["kvm-amd"];
-  boot.extraModulePackages = [ config.hardware.nvidia.package ];
+  # add nvidia x11 drivers
+  # TODO: not sure if needed
+  # https://nixos.wiki/wiki/Nvidia#Booting_to_Text_Mode
+  boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11_beta ];
+
+  # TODO: look in to this
+  boot.kernelParams = ["nvidia_drm.fbdev=1"
+"nvidia.NVreg_UsePageAttributeTable=1" ];
 
   boot.loader = {
     systemd-boot = {
@@ -34,4 +49,16 @@
 
   networking.useDHCP = lib.mkDefault true;
   # disk configuration managed by disko
+
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.graphics.enable = true;
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    open = true;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    powerManagement.enable = true;
+  };
+  #hardware.nvidia-container-toolkit.enable = true;
 }
